fd7c40fe2f554240d947187874aae1a03a9aeda6
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-11-07 12:06:21 -0500

    demo other class setup

--- src/disp_s1/product_info.py
+++ src/disp_s1/product_info.py
@@ -1,85 +1,79 @@
-from dataclasses import dataclass, fields
+from dataclasses import dataclass
 
 import numpy as np
-from numpy.typing import DTypeLike
+from dolphin import io
+from dolphin._types import Filename
+from numpy.typing import ArrayLike, DTypeLike
 
 
-@dataclass(frozen=True)
-class DispProductInfo:
-    """Describe me plz."""
-
+@dataclass
+class ProductInfo:
     name: str
     description: str
     fillvalue: DTypeLike
     attrs: dict
 
-    @classmethod
-    def unwrapped_phase(cls):
-        return cls(
-            name="unwrapped_phase",
-            description="Unwrapped phase",
-            fillvalue=np.nan,
-            attrs=dict(units="radians"),
+
+unwrapped_phase_info = ProductInfo(
+    name="unwrapped_phase",
+    description="Unwrapped phase",
+    fillvalue=np.nan,
+    attrs=dict(units="radians"),
+)
+
+connected_component_labels = ProductInfo(
+    name="connected_component_labels",
+    description="Connected component labels of the unwrapped phase",
+    fillvalue=0,
+    attrs=dict(units="unitless"),
+)
+
+temporal_coherence = ProductInfo(
+    name="temporal_coherence",
+    description="Temporal coherence of phase inversion",
+    fillvalue=np.nan,
+    attrs=dict(units="unitless"),
+)
+
+interferometric_correlation = ProductInfo(
+    name="interferometric_correlation",
+    description=(
+        "Estimate of interferometric correlation derived from"
+        " multilooked interferogram."
+    ),
+    fillvalue=np.nan,
+    attrs=dict(units="unitless"),
+)
+
+persistent_scattere_mask = ProductInfo(
+    name="persistent_scatterer_mask",
+    description=(
+        "Mask of persistent scatterers downsampled to the multilooked output grid."
+    ),
+    fillvalue=255,
+    attrs=dict(units="unitless"),
+)
+
+
+class DispProductLayer:
+    def __init__(
+        self, filename: Filename, product_info: ProductInfo, masked: bool = False
+    ):
+        self.filename = filename
+        self.product_info = product_info
+        self.data: ArrayLike = io.load_gdal(filename, masked=masked)
+
+
+class UnwrappedPhaseLayer(DispProductLayer):
+    def __init__(self, filename: Filename):
+        super().__init__(
+            filename=filename, product_info=unwrapped_phase_info, masked=True
         )
+        self.data = np.ma.filled(self.data, 0)
 
-    @classmethod
-    def connected_component_labels(cls):
-        return cls(
-            name="connected_component_labels",
-            description="Connected component labels of the unwrapped phase",
-            fillvalue=0,
-            attrs=dict(units="unitless"),
+
+class ConnectedComponentLabelsLayer(DispProductLayer):
+    def __init__(self, filename: Filename):
+        super().__init__(
+            filename=filename, product_info=unwrapped_phase_info, masked=True
         )
-
-    @classmethod
-    def temporal_coherence(cls):
-        return cls(
-            name="temporal_coherence",
-            description="Temporal coherence of phase inversion",
-            fillvalue=np.nan,
-            attrs=dict(units="unitless"),
-        )
-
-    @classmethod
-    def interferometric_correlation(cls):
-        return cls(
-            name="interferometric_correlation",
-            description=(
-                "Estimate of interferometric correlation derived from"
-                " multilooked interferogram."
-            ),
-            fillvalue=np.nan,
-            attrs=dict(units="unitless"),
-        )
-
-    @classmethod
-    def persistent_scattere_mask(cls):
-        return cls(
-            name="persistent_scatterer_mask",
-            description=(
-                "Mask of persistent scatterers downsampled to the multilooked"
-                " output grid."
-            ),
-            fillvalue=255,
-            attrs=dict(units="unitless"),
-        )
-
-
-@dataclass(frozen=True)
-class DispProductsInfo:
-    """Describe me plz."""
-
-    unwrapped_phase: DispProductInfo = DispProductInfo.unwrapped_phase()
-    connected_component_labels: DispProductInfo = (
-        DispProductInfo.connected_component_labels()
-    )
-    temporal_coherence: DispProductInfo = DispProductInfo.temporal_coherence()
-    interferometric_correlation: DispProductInfo = (
-        DispProductInfo.interferometric_correlation()
-    )
-    persistent_scattere_mask: DispProductInfo = (
-        DispProductInfo.persistent_scattere_mask()
-    )
-
-    def as_list(self):
-        return [getattr(self, field) for field in fields(self)]
